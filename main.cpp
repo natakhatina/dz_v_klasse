676d5e58efe2ca2aa338ba974f46990e79da296d
Pablitosi4 <pasha.fetisov.96@mail.ru>
2018-11-30 17:51:23 +0300

    Домашка

--- main.cpp
+++ main.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
+
 class BaseA{
+private:
+
 public:
-    virtual float getA()=0;
+   virtual float getA()=0;
 };
 
+
 class BaseB{
+private:
+
 public:
     virtual float getB()=0;
 };
+
 
 class core{
     BaseA* a;
     BaseB* b;
 public:
     core(BaseA*_a,BaseB*_b):a(_a),b(_b){}
+
     float calc(){
         return a->getA()+b->getB();
     };
 };
+
+class BaseA_dop: public BaseA{
+private:
+
+public:
+    float getA(){
+        return 2.03;
+    }
+
+};
+
+
+class BaseB_dop: public BaseB {
+private:
+
+public:
+    float getB(){
+        return 1.11;
+    }
+
+};
+
+
 int main() {
-    core my_core(,);
+    core my_core(new  BaseA_dop, new  BaseB_dop);
     std::cout<<my_core.calc()<<'\n';
     return 0;
 }
+
+
+
+
+
